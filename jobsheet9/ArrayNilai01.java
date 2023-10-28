d2bd2bfaa21207e92ef258c0157497286d99c981
Anuzuluzzuhdi <abhinaya.zuhdi@gmail.com>
2023-10-29 01:00:32 +0700

    ModifikasiPercobaan2

--- jobsheet9/ArrayNilai01.java
+++ jobsheet9/ArrayNilai01.java
@@ -4,13 +4,17 @@ public class ArrayNilai01 {
         Scanner sc01 = new Scanner(System.in);
         int[] NilaiAkhir = new int[10];
 
-        for (int i = 0; i< 10; i++){
+        for (int i = 0; i< NilaiAkhir.length; i++){
             System.out.println("Masukkan nilai akhir ke-"+ i + " : ");
             NilaiAkhir[i] = sc01.nextInt();
         }
 
         for (int i =0; i< 10; i++){
-            System.out.println("Nilai akhir ke-"+ i + " adalah "+NilaiAkhir[i]);
+            if (NilaiAkhir[i] > 70){
+            System.out.println("Mahasiswa ke-"+ i + " lulus! ");
+            } else {
+                System.out.println("Mahasiswa ke-"+ i + " tidak lulus! ");
+            }
         }
     }
 }
