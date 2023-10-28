c0560f60f79131e8eec2ddadd60727a38f9ff8f4
Anuzuluzzuhdi <abhinaya.zuhdi@gmail.com>
2023-10-29 03:30:20 +0700

    modifikasi percobaan4

--- jobsheet9/LinearSearch01.java
+++ jobsheet9/LinearSearch01.java
@@ -1,16 +1,30 @@
 import java.util.Scanner;
 public class LinearSearch01 {
     public static void main(String[] args) {
-        int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-        int key = 20;
-        int hasil = 0;
+        Scanner sc01 = new Scanner(System.in);
+
+        System.out.println("Masukkan jumlah elemen array :");
+        int jumlahElemen = sc01.nextInt();
+        int[] arrayInt = new int[jumlahElemen];
 
         for (int i = 0; i <arrayInt.length; i++){
+            System.out.println("Masukkan elemen array ke-"+i);
+            arrayInt[i] = sc01.nextInt();
+        }
+        System.out.println("Masukkan key yang ingin dicari : ");
+        int key = sc01.nextInt();
+
+        int hasil = -1;
+        for (int i = 0; i < jumlahElemen; i++){
             if (arrayInt[i] == key){
                 hasil = i;
                 break;
             }
         }
-        System.out.println("Key ada dalam array pada posisi indeks ke-"+hasil);
+        if (hasil != -1){
+            System.out.println("Key ada dalam array pada posisi indeks ke-"+hasil);
+        } else {
+            System.out.println("Key tidak ditemukan");
+        }
     }
 }
