432cd78a08bf0b3b74286aaf5569ca2b4edd9617
Anuzuluzzuhdi <abhinaya.zuhdi@gmail.com>
2023-11-02 14:38:30 +0700

    modifikasi.6

--- jobsheet10/bioskop01.java
+++ jobsheet10/bioskop01.java
@@ -17,8 +17,8 @@ public class bioskop01{
 
         System.out.println(penonton.length);
         
-        for (int i = 0; i < penonton.length; i++){
-            System.out.println("Panjang baris ke-" + (i+i) + ": " + penonton[i].length);
+        for (String[] barisPenonton : penonton){
+            System.out.println("Panjang baris: " + barisPenonton.length);
         }
     }
 }
