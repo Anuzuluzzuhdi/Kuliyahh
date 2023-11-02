2bf6a5e2f52494ef7ed86a362b87ffa957529e56
Anuzuluzzuhdi <abhinaya.zuhdi@gmail.com>
2023-11-02 14:34:02 +0700

    modifikasi.5

--- jobsheet10/bioskop01.java
+++ jobsheet10/bioskop01.java
@@ -15,9 +15,10 @@ public class bioskop01{
         penonton[3][0] = "Gisel";
         penonton[3][1] = "Hana";
 
-        System.out.printf("%s \t %s\n", penonton[0][0], penonton [0][1]);
-        System.out.printf("%s \t %s\n", penonton[1][0], penonton [1][1]);
-        System.out.printf("%s \t %s\n", penonton[2][0], penonton [2][1]);
-        System.out.printf("%s \t %s\n", penonton[3][0], penonton [3][1]);
+        System.out.println(penonton.length);
+        
+        for (int i = 0; i < penonton.length; i++){
+            System.out.println("Panjang baris ke-" + (i+i) + ": " + penonton[i].length);
         }
     }
+}
