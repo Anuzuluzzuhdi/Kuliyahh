322ae04c03f728568f143dd966a14405e32baa88
Anuzuluzzuhdi <abhinaya.zuhdi@gmail.com>
2023-11-09 13:31:56 +0700

    Percobaan2 DONE

---
+++ jobsheet11/kasus1.java
@@ -0,0 +1,10 @@
+public class kasus1 {
+    public static void main(String[] args) {
+        for (int i = 0; i<4; i++) {
+            for (int j= 0; j < 4-1-i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
