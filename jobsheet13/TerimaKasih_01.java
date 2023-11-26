6e0cc360d3f18d90704ba4360e434441754c33f0
Anuzuluzzuhdi <abhinaya.zuhdi@gmail.com>
2023-11-26 11:00:03 +0700

    fungsi UcapanTambahan_Percobaan2

--- jobsheet13/TerimaKasih_01.java
+++ jobsheet13/TerimaKasih_01.java
@@ -1,7 +1,14 @@
 public class TerimaKasih_01 {
-    
+    public static void UcapanTerimaKasih() {
+    System.out.println("Thank you for being the best mother in the world.\n"+
+    "You support me for everything and always wish me the best.");
+    }
+    public static void UcapanTambahan(String ucapan) {
+        System.out.println(ucapan);
+    }
     public static void main(String[] args) {
-        System.out.println("Thank you for being the best mother in the world.\n"+
-        "You support me for everything and always wish me the best.");
+        UcapanTerimaKasih();
+        String ucapan = "Terima Kasih Pak... Buk.. Semoga Sehat Selalu";
+        UcapanTambahan(ucapan);
     }
 }
