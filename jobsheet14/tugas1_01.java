e89adc1fbd2e6e2b42c827cd73c90eeba568d5a8
Anuzuluzzuhdi <abhinaya.zuhdi@gmail.com>
2023-12-03 01:11:43 +0700

    JS 14 part 2 (tugas 1,3,4)

---
+++ jobsheet14/tugas1_01.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class tugas1_01 {
+    public static void deretDescendingRekursif(int a) {
+        for(int i = a; i >= 0; i--) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void deretDescendingIteratif(int a) {
+        if(a >= 0) {
+            System.out.print(a + " ");
+            deretDescendingIteratif(a - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc01 = new Scanner(System.in);
+        System.out.println("Masukkan angka :");
+        int a = sc01.nextInt();
+
+        System.out.println("Rekursif :");
+        deretDescendingRekursif(a);
+
+        System.out.println("\nIteratif :");
+        deretDescendingIteratif(a);
+        
+    }
+}
---
+++ jobsheet14/tugas4_01.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+public class tugas4_01 {
+    public static int primaRekursif(int a, int i) {
+        
+    }
+}
